19d40db322d361fc0c4dbae3b7e090248b8e413d
Dominic D <17803165+DomicidalDesigns@users.noreply.github.com>
2019-07-05 13:55:50 -0400

    Better algorithm with reduced amount of blue light

--- CT2RGB.py
+++ CT2RGB.py
@@ -5,6 +5,15 @@
 import math
 
 def CT2RGB(temperature):
+    # note: I noticed the blue LED was much brighter than the red and green LEDs
+    # To fix this, I reduced the intensity of the blue light
+    blue_a=55 #adjustment value for blue amount function
+    #blue_a=138.5177312231 #default value
+    blue_b=120 # adjustment value for blue light function
+    #blue_b=305.0447927307 #default value
+    maxBlue=100 # The maximum blue RGB value
+    #maxBlue=255 #default value
+
     temperature=temperature/100
     #Calculate red
     if temperature <= 66:
@@ -12,6 +21,7 @@ def CT2RGB(temperature):
     else:
         red = temperature - 60
         red = 329.698727446 * (red ** -0.1332047592)
+        red=255
         if red < 0:
             red = 0
         if red > 255:
@@ -33,16 +43,15 @@ def CT2RGB(temperature):
             green = 255
     #Calculate blue
     if temperature >= 66:
-        blue = 255
+        blue = maxBlue
     else:
         if temperature <= 19:
             blue = 0
         else:
             blue = temperature - 10
-            blue = (138.5177312231 * math.log(blue)) - 305.0447927307
+            blue = (blue_a * math.log(blue)) - blue_b
             if blue < 0:
                 blue = 0
-            if blue > 255:
-                blue = 255
+            if blue > maxBlue:
+                blue = maxBlue
     return '%02x%02x%02x' %(int(red),int(green),int(blue))
-print(CT2RGB(1200))
